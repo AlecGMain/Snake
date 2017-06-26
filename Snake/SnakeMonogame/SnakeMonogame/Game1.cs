56fd5ff319f8cb72ff5a90f4422d83326e59a30c
AlecGMain <geosimonianalec@gmail.com>
2017-06-26 16:53:23 -0700

    Inital commit

---
+++ Snake/SnakeMonogame/SnakeMonogame/Game1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SnakeMonogame
+{
+   
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+        Texture2D image;
+        Vector2 position;
+        Color tint;
+  
+        protected override void Initialize()
+        {
+           
+            base.Initialize();
+        }
+
+     
+        protected override void LoadContent()
+        {
+          
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            image = Content.Load<Texture2D>("Tower1");
+            position = new Vector2(0, 0);
+            tint = Color.White;
+         
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+      
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(image, position, tint);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
